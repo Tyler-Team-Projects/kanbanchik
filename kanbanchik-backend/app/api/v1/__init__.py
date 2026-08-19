bd553638c4223669ac766c6b6e41849fba9680fe
Akhmetov Igor <mcventik@yandex.ru>
2026-08-19 15:50:42 +0400

    fix: worked app with dishka for users model; stable version of now

--- kanbanchik-backend/app/api/v1/__init__.py
+++ kanbanchik-backend/app/api/v1/__init__.py
@@ -4,7 +4,11 @@ from app.modules.users.router import router as users_router
 
 router = APIRouter()
 <<<<<<< HEAD
+<<<<<<< HEAD
 router.include_router(users_router)
 =======
 router.include_router(users_router, prefix="/users")
 >>>>>>> d5e46f4 (feat: add first model and work schema for try start project with dishka)
+=======
+router.include_router(users_router)
+>>>>>>> 7c2a80d (fix: worked app with dishka for users model; stable version of now)
--- kanbanchik-backend/app/modules/users/provider.py
+++ kanbanchik-backend/app/modules/users/provider.py
@@ -10,6 +10,12 @@ class UsersProvider(Provider):
     @provide(scope=Scope.REQUEST, provides=IUserService)
     async def get_user_service(self, repo: IUserRepository) -> IUserService:
         return UserService(repo)
+<<<<<<< HEAD
+=======
+    @provide(scope=Scope.REQUEST)
+    async def get_user_repo(self, session: AsyncSession) -> IUserRepository:
+        return UserRepository(session)
+>>>>>>> 7c2a80d (fix: worked app with dishka for users model; stable version of now)
 
     @provide(scope=Scope.REQUEST)
     async def get_user_repo(self, session: AsyncSession) -> IUserRepository:
