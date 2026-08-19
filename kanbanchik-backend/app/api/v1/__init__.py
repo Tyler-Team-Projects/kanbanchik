f6490a4deee3e786f3be365edb6a24b7d2dc3ca8
Akhmetov Igor <mcventik@yandex.ru>
2026-08-19 09:51:32 +0400

    feat: add first model and work schema for try start project with dishka

--- kanbanchik-backend/app/api/v1/__init__.py
+++ kanbanchik-backend/app/api/v1/__init__.py
@@ -3,4 +3,8 @@ from fastapi import APIRouter
 from app.modules.users.router import router as users_router
 
 router = APIRouter()
+<<<<<<< HEAD
 router.include_router(users_router)
+=======
+router.include_router(users_router, prefix="/users")
+>>>>>>> d5e46f4 (feat: add first model and work schema for try start project with dishka)
