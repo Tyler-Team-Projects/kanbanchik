79a79554d8ae0692ef27bca6f40d3a59a4be825e
Akhmetov Igor <mcventik@yandex.ru>
2026-08-22 13:39:02 +0400

    fix: merge changes after pull

--- kanbanchik-backend/app/api/v1/__init__.py
+++ kanbanchik-backend/app/api/v1/__init__.py
@@ -2,12 +2,4 @@ from fastapi import APIRouter
 from app.modules.users.router import router as users_router
 
 router = APIRouter()
-<<<<<<< HEAD
-<<<<<<< HEAD
 router.include_router(users_router)
-=======
-router.include_router(users_router, prefix="/users")
->>>>>>> d5e46f4 (feat: add first model and work schema for try start project with dishka)
-=======
-router.include_router(users_router)
->>>>>>> 7c2a80d (fix: worked app with dishka for users model; stable version of now)
